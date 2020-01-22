ca7f8e4647a5a8835435831ee339ba3e882fc4f4
Hartman <52998392+tebenkov2222@users.noreply.github.com>
2020-01-23 00:28:04 +0400

    Create

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public GameObject _GOPlayer;
+    public Vector3 _posPlayer;
+    private float _maxJump = 1060, 
+        _speed, _speedstandart = 20, _speedSitDown = 10, NormalGravityScale = 7;
+    bool _JumpBool = false, _LeftBool = false, _RightBool = false, _DownBool = false, _StopBlock = false;
+    public bool _RightSide = false, _LeftSide = false, _DownSide = false, _RightBlc = false, _LeftBlc = false; [HideInInspector]
+    void Start()
+    {
+        _speed = _speedstandart;
+        _posPlayer = _GOPlayer.transform.position;
+        _GOPlayer.GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+    void FixedUpdate()
+    {
+        _posPlayer = _GOPlayer.transform.position;
+        if (_JumpBool)
+        {
+            if (_DownSide)
+            {
+                _GOPlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1) * _maxJump);
+            }
+            else
+            {
+                if (_RightBlc || _LeftBlc)
+                {
+
+                    _GOPlayer.GetComponent<Rigidbody2D>().gravityScale = 0;
+                    _GOPlayer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    _StopBlock = true;
+                }
+                else
+                {
+                    _GOPlayer.GetComponent<Rigidbody2D>().gravityScale = NormalGravityScale;
+                }
+                if ((_RightSide || _StopBlock )&& _LeftBool)
+                {
+                    _StopBlock = false;
+                    _GOPlayer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    _GOPlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.1f, 1f) * _maxJump);
+                }
+                if ((_LeftSide || _StopBlock) && _RightBool)
+                {
+                    _StopBlock = false;
+                    _GOPlayer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    _GOPlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.1f, 1f) * _maxJump);
+                }
+            }
+        }
+        else
+        {
+            _StopBlock = false;
+            _GOPlayer.GetComponent<Rigidbody2D>().gravityScale = NormalGravityScale;
+        }
+        if (_LeftBool && !_LeftSide)
+        {
+            _GOPlayer.transform.position += new Vector3(-0.01f * _speed, 0, 0);
+        }
+        if (_RightBool && !_RightSide)
+        {
+            _GOPlayer.transform.position += new Vector3(0.01f * _speed, 0, 0);
+        }
+        if (_DownBool)
+        {
+            _speed = _speedSitDown;
+        }
+        else
+        {
+            _speed = _speedstandart;
+        }
+    }
+    public void Jump()
+    {
+       _JumpBool = !_JumpBool;
+    }
+    public void MoveLeft()
+    {
+        _LeftBool = !_LeftBool;
+    }
+    public void MoveRight()
+    {
+        _RightBool = !_RightBool;
+    }
+    public void MoveSitDown()
+    {
+        _DownBool = !_DownBool;
+    }
+}
---
+++ Assets/Scripts/SodesPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SodesPlayer : MonoBehaviour
+{
+    public int mode = 1;
+    public GameObject player;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (mode == 1) player.GetComponent<MovePlayer>()._DownSide = true;
+        if (mode == 2) player.GetComponent<MovePlayer>()._RightSide = true;
+        if (mode == 3) player.GetComponent<MovePlayer>()._LeftSide = true;
+        if (collision.gameObject.tag == "StopBlock")
+        {
+            if (mode == 2) player.GetComponent<MovePlayer>()._RightBlc = true;
+            if (mode == 3) player.GetComponent<MovePlayer>()._LeftBlc = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (mode == 1) player.GetComponent<MovePlayer>()._DownSide = false;
+        if (mode == 2) player.GetComponent<MovePlayer>()._RightSide = false;
+        if (mode == 3) player.GetComponent<MovePlayer>()._LeftSide = false;
+        if (collision.gameObject.tag == "StopBlock")
+        {
+            if (mode == 2) player.GetComponent<MovePlayer>()._RightBlc = false;
+            if (mode == 3) player.GetComponent<MovePlayer>()._LeftBlc = false;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
